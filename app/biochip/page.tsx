ee79dc01e88bd7fee89693656b30a13fb0ee2622
ZxStim <thang.victor.luong@gmail.com>
2024-07-10 00:58:58 +0700

    edit dashboard style

--- app/biochip/page.tsx
+++ app/biochip/page.tsx
@@ -3,7 +3,7 @@ import BioChipImage from "@/components/biochip-image";
 
 export default function BioChip() {
   return (
-    <div className="flex flex-col gap-2 py-6 px-4 h-[768px]">
+    <div className="flex flex-col gap-2 py-6 pl-4 h-[768px]">
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-extrabold">biochip</h1>
         <p className="text-md text-muted-foreground">
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import DashboardContent from "@/components/dashboard-content";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-2 py-6 px-4 h-[768px]">
+    <div className="flex flex-col gap-2 py-6 pl-4 h-[768px]">
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-extrabold">dashboard</h1>
         <p className="text-md text-muted-foreground">the central command of your dream</p>
