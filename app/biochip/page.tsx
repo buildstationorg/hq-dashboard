81d8dd492be487bd3ca969f63f49f50ced677079
ZxStim <thang.victor.luong@gmail.com>
2024-07-10 22:44:26 +0700

    changed header layout in mobile

--- app/biochip/page.tsx
+++ app/biochip/page.tsx
@@ -6,7 +6,7 @@ import BiochipStash from "@/components/biochip-stash";
 
 export default function BioChip() {
   return (
-    <div className="flex flex-col gap-2 py-6 pl-4 lg:h-[768px]">
+    <div className="flex flex-col gap-2 py-6 px-4 lg:pl-4 h-full lg:h-[768px]">
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-extrabold">biochip</h1>
         <p className="text-md text-muted-foreground">
--- components/header.tsx
+++ components/header.tsx
@@ -2,7 +2,7 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export default function Header() {
   return (
-    <div className="flex flex-row justify-between items-center border-b-2 py-6 border-primary px-4">
+    <div className="flex flex-col gap-4 md:flex md:flex-row md:justify-between items-center border-b-2 pb-6 md:py-6 border-primary md:px-4">
       <h1 className="text-xl lg:text-4xl font-extrabold">buildstation<span className="text-xl lg:text-4xl font-medium bg-primary rounded-md text-secondary px-2 py-1 ml-2">HQ</span></h1>
       <ConnectButton />
     </div>
