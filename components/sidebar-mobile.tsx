35790c7a43a071d3d3a08c84e243d6bfcbc75069
ZxStim <thang.victor.luong@gmail.com>
2025-01-15 17:15:40 +0700

    fix: various fixes to pages

--- components/sidebar-mobile.tsx
+++ components/sidebar-mobile.tsx
@@ -2,9 +2,9 @@
 
 import { useState } from "react";
 import { usePathname } from "next/navigation";
-import Link, { LinkProps } from "next/link"
-import { useRouter } from "next/navigation"
-import { cn } from "@/lib/utils"
+import Link, { LinkProps } from "next/link";
+import { useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
 import {
   Sheet,
@@ -13,7 +13,16 @@ import {
   SheetContent,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import { LayoutDashboard, Cpu, Bug, ArrowRightFromLine } from "lucide-react";
+import {
+  LayoutDashboard,
+  Cpu,
+  SearchCode,
+  HandCoins,
+  Crosshair,
+  Laptop,
+  Briefcase,
+  ArrowRightFromLine,
+} from "lucide-react";
 
 export default function SidebarMobile() {
   const [open, setOpen] = useState(false);
@@ -39,7 +48,12 @@ export default function SidebarMobile() {
 
       <SheetContent side="left" className="pr-6">
         <SheetHeader>
-          <SheetTitle className="text-xl lg:text-4xl font-extrabold">buildstation<span className="text-xl lg:text-4xl font-medium bg-primary rounded-md text-secondary px-2 py-1 ml-2">HQ</span></SheetTitle>
+          <SheetTitle className="text-xl lg:text-4xl font-extrabold">
+            buildstation
+            <span className="text-xl lg:text-4xl font-medium bg-primary rounded-md text-secondary px-2 py-1 ml-2">
+              HQ
+            </span>
+          </SheetTitle>
         </SheetHeader>
         <div className="flex flex-col space-y-3 pt-6">
           <MobileLink
@@ -50,7 +64,7 @@ export default function SidebarMobile() {
             onOpenChange={setOpen}
           >
             <LayoutDashboard className="w-4 h-4 mr-2" />
-            dashboard
+            Dashboard
           </MobileLink>
           <MobileLink
             href="/biochip"
@@ -60,17 +74,57 @@ export default function SidebarMobile() {
             onOpenChange={setOpen}
           >
             <Cpu className="w-4 h-4 mr-2" />
-            biochip
+            Biochip
           </MobileLink>
           <MobileLink
-            href="/bounty"
+            href="/inspect"
             className={`flex flex-row items-center text-xl ${isActiveRoute(
-              "/bounty"
+              "/inspect"
             )} w-[190px]`}
             onOpenChange={setOpen}
           >
-            <Bug className="w-4 h-4 mr-2" />
-            bounty
+            <SearchCode className="w-4 h-4 mr-2" />
+            Inspect
+          </MobileLink>
+          <MobileLink
+            href="/grants"
+            className={`flex flex-row items-center text-xl ${isActiveRoute(
+              "/grants"
+            )} w-[190px]`}
+            onOpenChange={setOpen}
+          >
+            <HandCoins className="w-4 h-4 mr-2" />
+            Grants
+          </MobileLink>
+          <MobileLink
+            href="/bounties"
+            className={`flex flex-row items-center text-xl ${isActiveRoute(
+              "/bounties"
+            )} w-[190px]`}
+            onOpenChange={setOpen}
+          >
+            <Crosshair className="w-4 h-4 mr-2" />
+            Bounties
+          </MobileLink>
+          <MobileLink
+            href="/hackathon"
+            className={`flex flex-row items-center text-xl ${isActiveRoute(
+              "/hackathon"
+            )} w-[190px]`}
+            onOpenChange={setOpen}
+          >
+            <Laptop className="w-4 h-4 mr-2" />
+            Hackathon
+          </MobileLink>
+          <MobileLink
+            href="/jobs"
+            className={`flex flex-row items-center text-xl ${isActiveRoute(
+              "/jobs"
+            )} w-[190px]`}
+            onOpenChange={setOpen}
+          >
+            <Briefcase className="w-4 h-4 mr-2" />
+            Jobs
           </MobileLink>
         </div>
       </SheetContent>
@@ -79,9 +133,9 @@ export default function SidebarMobile() {
 }
 
 interface MobileLinkProps extends LinkProps {
-  onOpenChange?: (open: boolean) => void
-  children: React.ReactNode
-  className?: string
+  onOpenChange?: (open: boolean) => void;
+  children: React.ReactNode;
+  className?: string;
 }
 
 function MobileLink({
@@ -91,18 +145,18 @@ function MobileLink({
   children,
   ...props
 }: MobileLinkProps) {
-  const router = useRouter()
+  const router = useRouter();
   return (
     <Link
       href={href}
       onClick={() => {
-        router.push(href.toString())
-        onOpenChange?.(false)
+        router.push(href.toString());
+        onOpenChange?.(false);
       }}
       className={cn(className)}
       {...props}
     >
       {children}
     </Link>
-  )
+  );
 }
